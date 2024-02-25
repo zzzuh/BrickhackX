ef035d79390db6fcbdf3b5fb2db5670f8b274cfc
Alex <al9379@rit.edu>
2024-02-24 23:46:27 -0500

    balllss

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ImageUpload from './components/ImageUpload';
 import ImageDisplay from './components/ImageDisplay';
 import NavigatorBar from './components/NavigatorBar';
 import { createContext, useState } from 'react';
+import StateComponent from './components/State';
 
 export const ImageContext = createContext()
 
@@ -12,9 +13,12 @@ function App() {
     <div className=' flex h-screen flex-col'>
       <NavigatorBar></NavigatorBar>
       <div className='flex items-center justify-center h-screen flex-col'>
+        <div className="mb-4">
+          <StateComponent></StateComponent>
+        </div>
         <ImageContext.Provider value={{image, setImage}}>
-          <ImageUpload></ImageUpload>
-          <ImageDisplay></ImageDisplay>
+        <ImageUpload></ImageUpload>
+        <ImageDisplay></ImageDisplay>
         </ImageContext.Provider>
       </div>
     </div>
